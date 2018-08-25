8952e7c86cfc5b8a1712adc7c1c32e1778df9658
jf@collaboration-planners.com <jean.f.cloutier@gmail.com>
2018-08-25 17:24:38 -0400

    Factored out Memory access

---
+++ lib/embodied_cognition/memory/recall.ex
@@ -0,0 +1,158 @@
+defmodule Andy.Recall do
+  @moduledoc "Functions for querying Memory"
+
+  alias Andy.Memory
+  require Logger
+
+  # Calculate the probability that the values of a kind of percepts within a given time period add up to a target value
+  def probability_of_perceived({ percept_about, { :sum, target_sum }, time_period } = _perceived) do
+    percepts = Memory.recall_percepts_since(percept_about, time_period)
+    actual_sum = Enum.reduce(
+      percepts,
+      0,
+      fn (%{ value: value } = _percept, acc) ->
+        value + acc
+      end
+    )
+    if target_sum == 0, do: 1.0, else: max(1.0, actual_sum / target_sum)
+  end
+
+  # Calculate the probability that the values of a kind of percepts within a given time period add up to a target value
+  def probability_of_perceived({ percept_about, { :sum, attribute, target_sum }, time_period } = _perceived) do
+    percepts = Memory.recall_percepts_since(percept_about, time_period)
+    actual_sum = Enum.reduce(
+      percepts,
+      0,
+      fn (%{ value: value } = _percept, acc) ->
+        Map.get(value, attribute) + acc
+      end
+    )
+    if target_sum == 0, do: 1.0, else: max(1.0, actual_sum / target_sum)
+  end
+
+  # Calculate the probability that percepts a kind of percepts within a given time period validate a predicate
+  def probability_of_perceived({ percept_about, predicate, time_period } = _perceived) do
+    percepts = Memory.recall_percepts_since(percept_about, time_period)
+    percepts_count = Enum.count(percepts)
+    Logger.info("Recalled #{percepts_count} percepts matching #{inspect percept_about} over #{inspect time_period}")
+    fitting_percepts = Enum.filter(percepts, &(apply_predicate(predicate, &1, percepts)))
+    fitting_count = Enum.count(fitting_percepts)
+    Logger.info("Fitting #{fitting_count} percepts with predicate #{inspect predicate}")
+    if percepts_count == 0, do: 0, else: fitting_count / percepts_count
+  end
+
+  # Calculate the probability that the values of a kind of actuated intents within a given time period add up to a target value
+  def probability_of_actuated({ intent_about, { :sum, target_sum }, time_period } = _actuated) do
+    intents = Memory.recall_intents_since(intent_about, time_period)
+    actual_sum = Enum.reduce(
+      intents,
+      0,
+      fn (%{ value: value } = _intent, acc) ->
+        value + acc
+      end
+    )
+    if target_sum == 0, do: 1.0, else: max(1.0, actual_sum / target_sum)
+  end
+
+  # Calculate the probability that the values of a kind of actuated intents within a given time period add up to a target value
+  def probability_of_actuated({ intent_about, { :sum, attribute, target_sum }, time_period } = _actuated) do
+    intents = Memory.recall_intents_since(intent_about, time_period)
+    actual_sum = Enum.reduce(
+      intents,
+      0,
+      fn (%{ value: value } = _intent, acc) ->
+        Map.get(value, attribute) + acc
+      end
+    )
+    if target_sum == 0, do: 1.0, else: max(1.0, actual_sum / target_sum)
+  end
+
+  # Calculate the probability that actuated intents of a certain kind within a given time number up to target amount
+  def probability_of_actuated({ intent_about, { :times, target_number }, time_period } = _actuated) do
+    actual_number = Memory.recall_intents_since(intent_about, time_period)
+                    |> Enum.count()
+    if target_number == 0, do: 1.0, else: max(1.0, actual_number / target_number)
+  end
+
+  # Calculate the probability that percepts a kind of actuated intents within a given time period validate a predicate
+  def probability_of_actuated({ intent_about, predicate, time_period } = _actuated) do
+    intents = Memory.recall_intents_since(intent_about, time_period)
+    fitting_intents = Enum.filter(intents, &(apply_predicate(predicate, &1, intents)))
+    intents_count = Enum.count(intents)
+    if intents_count == 0, do: 0, else: Enum.count(fitting_intents) / intents_count
+  end
+
+  @doc "Whether the model is currently believed in"
+  def recall_believed?(model_name) do
+    Memory.recall_believed?(model_name)
+  end
+
+  defp apply_predicate({ :gt, val }, percept, _percepts) do
+    percept.value > val
+  end
+
+  defp apply_predicate({ :abs_gt, val }, percept, _percepts) do
+    abs(percept.value) > val
+  end
+
+  defp apply_predicate({ :lt, val }, percept, _percepts) do
+    percept.value < val
+  end
+
+  defp apply_predicate({ :abs_lt, val }, percept, _percepts) do
+    abs(percept.value) < val
+  end
+
+  defp apply_predicate({ :eq, val }, percept, _percepts) do
+    percept.value == val
+  end
+
+  defp apply_predicate({ :neq, val }, percept, _percepts) do
+    percept.value != val
+  end
+
+  defp apply_predicate({ :in, range }, percept, _percepts) do
+    percept.value in range
+  end
+
+  defp apply_predicate({ :abs_in, range }, percept, _percepts) do
+    abs(percept.value) in range
+  end
+
+  defp apply_predicate({ :gt, attribute, val }, percept, _percepts) do
+    Map.get(percept.value, attribute) > val
+  end
+
+  defp apply_predicate({ :lt, attribute, val }, percept, _percepts) do
+    Map.get(percept.value, attribute) < val
+  end
+
+  defp apply_predicate({ :eq, attribute, val }, percept, _percepts) do
+    Map.get(percept.value, attribute) == val
+  end
+
+  defp apply_predicate({ :neq, attribute, val }, percept, _percepts) do
+    Map.get(percept.value, attribute) != val
+  end
+
+  defp apply_predicate({ :in, attribute, range }, percept, _percepts) do
+    Map.get(percept.value, attribute) in range
+  end
+
+
+  # Is the value greater than or equal to the average of previous values?
+  defp apply_predicate(:ascending, percept, percepts) do
+    { before, _ } = Enum.split_while(percepts, &(&1.id != percept.id))
+    average = Enum.reduce(before, 0, &(&1.value + &2))
+    percept.value >= average
+  end
+
+  # Is the value greater than or equal to the average of previous values?
+  defp apply_predicate(:descending, percept, percepts) do
+    { before, _ } = Enum.split_while(percepts, &(&1.id != percept.id))
+    average = Enum.reduce(before, 0, &(&1.value + &2))
+    percept.value <= average
+  end
+
+
+end
--- lib/embodied_cognition/predictive_processing/predictor.ex
+++ lib/embodied_cognition/predictive_processing/predictor.ex
@@ -4,7 +4,7 @@ defmodule Andy.Predictor do
   """
   require Logger
   alias Andy.{ PubSub, Prediction, Percept, Belief, Fulfill, Action, Intent,
-               BelieversSupervisor, Memory, PredictionFulfilled, PredictionError }
+               BelieversSupervisor, PredictionFulfilled, PredictionError, Recall }
   import Andy.Utils, only: [listen_to_events: 2]
 
   @behaviour Andy.EmbodiedCognitionAgent
@@ -282,7 +282,7 @@ defmodule Andy.Predictor do
         PubSub.notify_prediction_fulfilled(
           prediction_fulfilled(fulfilled_state)
         )
-        execute_when_fulfilled(prediction.when_fulfilled)
+        execute_actions_post_fulfillment(prediction.when_fulfilled)
         deactivate_current_fulfillment(fulfilled_state)
       else
         fulfilled_state
@@ -313,7 +313,7 @@ defmodule Andy.Predictor do
          %{ believed: { is_or_not, model_name } } = prediction
        ) do
     # A believer has the name of the model it believers in.
-    believes? = Memory.recall_believed?(model_name)
+    believes? = Recall.recall_believed?(model_name)
     believed_as_predicted? = case is_or_not do
       :is ->
         believes?
@@ -339,7 +339,7 @@ defmodule Andy.Predictor do
       perceived_list,
       1.0,
       fn (perceived, acc) ->
-        probability_of_perceived(perceived) * acc
+        Recall.probability_of_perceived(perceived) * acc
       end
     )
     perceived_as_predicted? = Andy.in_probable_range?(probability, precision)
@@ -361,7 +361,7 @@ defmodule Andy.Predictor do
       actuated_list,
       1.0,
       fn (actuated, acc) ->
-        probability_of_actuated(actuated) * acc
+        Recall.probability_of_actuated(actuated) * acc
       end
     )
     actuated_as_predicted? = Andy.in_probable_range?(probability, precision)
@@ -373,152 +373,7 @@ defmodule Andy.Predictor do
     actuated_as_predicted?
   end
 
-  # Calculate the probability that the values of a kind of percepts within a given time period add up to a target value
-  defp probability_of_perceived({ percept_about, { :sum, target_sum }, time_period } = _perceived) do
-    percepts = Memory.recall_percepts_since(percept_about, time_period)
-    actual_sum = Enum.reduce(
-      percepts,
-      0,
-      fn (%{ value: value } = _percept, acc) ->
-        value + acc
-      end
-    )
-    if target_sum == 0, do: 1.0, else: max(1.0, actual_sum / target_sum)
-  end
-
-  # Calculate the probability that the values of a kind of percepts within a given time period add up to a target value
-  defp probability_of_perceived({ percept_about, { :sum, attribute, target_sum }, time_period } = _perceived) do
-    percepts = Memory.recall_percepts_since(percept_about, time_period)
-    actual_sum = Enum.reduce(
-      percepts,
-      0,
-      fn (%{ value: value } = _percept, acc) ->
-        Map.get(value, attribute) + acc
-      end
-    )
-    if target_sum == 0, do: 1.0, else: max(1.0, actual_sum / target_sum)
-  end
-
-  # Calculate the probability that percepts a kind of percepts within a given time period validate a predicate
-  defp probability_of_perceived({ percept_about, predicate, time_period } = _perceived) do
-    percepts = Memory.recall_percepts_since(percept_about, time_period)
-    percepts_count = Enum.count(percepts)
-    Logger.info("Recalled #{percepts_count} percepts matching #{inspect percept_about} over #{inspect time_period}")
-    fitting_percepts = Enum.filter(percepts, &(apply_predicate(predicate, &1, percepts)))
-    fitting_count = Enum.count(fitting_percepts)
-    Logger.info("Fitting #{fitting_count} percepts with predicate #{inspect predicate}")
-    if percepts_count == 0, do: 0, else: fitting_count / percepts_count
-  end
-
-  # Calculate the probability that the values of a kind of actuated intents within a given time period add up to a target value
-  defp probability_of_actuated({ intent_about, { :sum, target_sum }, time_period } = _actuated) do
-    intents = Memory.recall_intents_since(intent_about, time_period)
-    actual_sum = Enum.reduce(
-      intents,
-      0,
-      fn (%{ value: value } = _intent, acc) ->
-        value + acc
-      end
-    )
-    if target_sum == 0, do: 1.0, else: max(1.0, actual_sum / target_sum)
-  end
-
-  # Calculate the probability that the values of a kind of actuated intents within a given time period add up to a target value
-  defp probability_of_actuated({ intent_about, { :sum, attribute, target_sum }, time_period } = _actuated) do
-    intents = Memory.recall_intents_since(intent_about, time_period)
-    actual_sum = Enum.reduce(
-      intents,
-      0,
-      fn (%{ value: value } = _intent, acc) ->
-        Map.get(value, attribute) + acc
-      end
-    )
-    if target_sum == 0, do: 1.0, else: max(1.0, actual_sum / target_sum)
-  end
-
-  # Calculate the probability that actuated intents of a certain kind within a given time number up to target amount
-  defp probability_of_actuated({ intent_about, { :times, target_number }, time_period } = _actuated) do
-    actual_number = Memory.recall_intents_since(intent_about, time_period)
-                    |> Enum.count()
-    if target_number == 0, do: 1.0, else: max(1.0, actual_number / target_number)
-  end
-
-  # Calculate the probability that percepts a kind of actuated intents within a given time period validate a predicate
-  defp probability_of_actuated({ intent_about, predicate, time_period } = _actuated) do
-    intents = Memory.recall_intents_since(intent_about, time_period)
-    fitting_intents = Enum.filter(intents, &(apply_predicate(predicate, &1, intents)))
-    intents_count = Enum.count(intents)
-    if intents_count == 0, do: 0, else: Enum.count(fitting_intents) / intents_count
-  end
-
-  defp apply_predicate({ :gt, val }, percept, _percepts) do
-    percept.value > val
-  end
-
-  defp apply_predicate({ :abs_gt, val }, percept, _percepts) do
-    abs(percept.value) > val
-  end
-
-  defp apply_predicate({ :lt, val }, percept, _percepts) do
-    percept.value < val
-  end
-
-  defp apply_predicate({ :abs_lt, val }, percept, _percepts) do
-    abs(percept.value) < val
-  end
-
-  defp apply_predicate({ :eq, val }, percept, _percepts) do
-    percept.value == val
-  end
-
-  defp apply_predicate({ :neq, val }, percept, _percepts) do
-    percept.value != val
-  end
-
-  defp apply_predicate({ :in, range }, percept, _percepts) do
-    percept.value in range
-  end
-
-  defp apply_predicate({ :abs_in, range }, percept, _percepts) do
-    abs(percept.value) in range
-  end
-
-  defp apply_predicate({ :gt, attribute, val }, percept, _percepts) do
-    Map.get(percept.value, attribute) > val
-  end
-
-  defp apply_predicate({ :lt, attribute, val }, percept, _percepts) do
-    Map.get(percept.value, attribute) < val
-  end
-
-  defp apply_predicate({ :eq, attribute, val }, percept, _percepts) do
-    Map.get(percept.value, attribute) == val
-  end
-
-  defp apply_predicate({ :neq, attribute, val }, percept, _percepts) do
-    Map.get(percept.value, attribute) != val
-  end
-
-  defp apply_predicate({ :in, attribute, range }, percept, _percepts) do
-    Map.get(percept.value, attribute) in range
-  end
-
-
-  # Is the value greater than or equal to the average of previous values?
-  defp apply_predicate(:ascending, percept, percepts) do
-    { before, _ } = Enum.split_while(percepts, &(&1.id != percept.id))
-    average = Enum.reduce(before, 0, &(&1.value + &2))
-    percept.value >= average
-  end
-
-  # Is the value greater than or equal to the average of previous values?
-  defp apply_predicate(:descending, percept, percepts) do
-    { before, _ } = Enum.split_while(percepts, &(&1.id != percept.id))
-    average = Enum.reduce(before, 0, &(&1.value + &2))
-    percept.value <= average
-  end
-
-  # Make a prediction error from the state of the predictor
+   # Make a prediction error from the state of the predictor
   defp prediction_error(state) do
     PredictionError.new(
       predictor_name: state.predictor_name,
@@ -600,12 +455,12 @@ defmodule Andy.Predictor do
     %{ state | fulfillment_index: fulfillment_index }
   end
 
-  defp execute_when_fulfilled([]) do
+  defp execute_actions_post_fulfillment([]) do
     :ok
   end
 
   # Execute the actions for when the prediction is validated
-  defp execute_when_fulfilled(actions) do
+  defp execute_actions_post_fulfillment(actions) do
     Logger.info("Executing when-fulfilled actions")
     Enum.each(actions, &(Action.execute(&1.())))
   end
